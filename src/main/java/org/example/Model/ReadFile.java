b72054020dcabd6c83d3e5c2a6f29581be99c04a
andreastif <45339645+andreastif@users.noreply.github.com>
2022-12-15 16:28:52 +0100

    Controller Branch small update

--- src/main/java/org/example/Model/ReadFile.java
+++ src/main/java/org/example/Model/ReadFile.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileReader {
+public class ReadFile {
 
     public List<Customer> createListFromFile(Path filePath) {
         int counter = 0;
--- src/main/java/org/example/Model/WriteFile.java
+++ src/main/java/org/example/Model/WriteFile.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class FileWriter {
+public class WriteFile {
     public void saveTransaction(String transaction) {
         try (BufferedWriter out = Files.newBufferedWriter(Path.of("src/main/resources/Transactions.txt"))) {
             out.write(transaction);
