3abb0b266b8c257cf41adcbc2966d0a642e149d0
alsomeb <alsomeb@gmail.com>
2022-12-15 15:41:59 +0100

    Collab Session Alex, Andreas, Felldén

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -27,6 +27,19 @@ public class Controller {
                 .findFirst().orElseThrow(() -> new NoSuchElementException());
     }
 
+
+    // TODO
+    public static String generateStringToTransactions(){
+        // Här ska vi bygga strängen som ska skickas in till FileWriter. Det enda FileWriter ska göra är att skriva till filen.
+        // Här bestämmer vi HUR den ska skriva till filen.
+
+        String completed = "";
+        StringBuilder sb = new StringBuilder();
+
+        return completed;
+    }
+
+    // TODO
     public static boolean transferToOtherAccount(int amountToSend, Customer toCustomer, Customer fromCustomer) {
         //ta in textfilen Customers.txt
         //validera id att skicka till
