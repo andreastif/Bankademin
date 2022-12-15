cb3ee00fe792732e4e4b44019daeb4f65f7d5e1c
andreastif <45339645+andreastif@users.noreply.github.com>
2022-12-15 17:22:45 +0100

    Lagt till Testmapp med tillhörande tester för att testa ny funktionalitet i controller.
    Controller uppdateringar:
     - nästan färdig isTransactionSuccessfull.
     - ny metod validateFunds.
    
    Varit tvungen att uppdatera utanför controller klassen med följande:
     - WriteFile (append)
     - Account (lagt till increaseBalance, decreaseBalance, samt getBalance)
     - Customer (lagt till getAccount)
    
    Döpt om FileWriter och FileReader till WriteFile och ReadFile pga krock med inbyggda javaklasser.

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -7,6 +7,7 @@ import org.example.Model.WriteFile;
 import org.example.View.*;
 
 
+import javax.swing.*;
 import java.nio.file.Path;
 import java.util.NoSuchElementException;
 
@@ -32,7 +33,7 @@ public class Controller {
     }
 
 
-    // TODO
+    // TODO - Prio 1, måste göras innan arbetet kan fortsätta
     public static String generateStringToTransactions(){
         // Här ska vi bygga strängen som ska skickas in till WriteFile. Det enda WriteFile ska göra är att skriva till filen.
         // Här bestämmer vi HUR den ska skriva till filen.
@@ -43,25 +44,40 @@ public class Controller {
         return completed;
     }
 
-    // TODO
-    public static boolean transferToOtherAccount(int amountToSend, Customer toCustomer, Customer fromCustomer) {
+    // TODO - Nästan klar.
+    public static boolean transferToOtherAccount(int amountToSend, Customer fromCustomer, Customer toCustomer) {
 
+        //ta in textfilen Customers.txt - OK (görs innan, ej här)
+        //validera id att skicka till - OK (görs innan, ej här)
+        //validera att belopp finns på egna kontot - OK
+        //skicka pengarna till andra kontot - OK
+            //sänk balance hos fromCustomer - OK
+            //öka balance hos toCustomer - OK
+            //TODO Skriv till transaktionsLoggen från den som har skickat, hur mycket och till vem amountToSend och spara till en transaktionsLogg.txt - EJ KLAR
+            //return true - OK
+        //return false om inte det går att skicka cash (fel toCustomer eller om insufficient funds etc) - OK
 
-        //ta in textfilen Customers.txt
-        //validera id att skicka till
-        //validera att belopp finns på egna kontot
-            //skicka pengarna till andra kontot =
-                //reducera det egna kontot
-                //öka det andra kontot
-                    //Om allt OK ->
-                    //Skriv till transaktionsLoggen från den som har skickat, hur mycket och till vem amountToSend och spara till en transaktionsLogg.txt
-                    //return true
-        //return false om inte det går att skicka cash (fel toCustomer eller om insufficient funds etc)
-        return false;
+        boolean isTransactionSuccessfull = false;
 
+        if (validateFunds(fromCustomer, amountToSend)) {
+            fromCustomer.getAccount().decreaseBalance(amountToSend);
+            toCustomer.getAccount().increaseBalance(amountToSend);
+
+            //endast i testsyfte
+            writeFile.saveTransaction("TEST"); //ska EGENTLIGEN anropa generateStringToTransactions, endast TEST!
+            isTransactionSuccessfull = true;
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Insufficient funds");
+        }
+        return isTransactionSuccessfull;
     }
 
-    // TODO
+    public static boolean validateFunds(Customer customer, int amount) {
+        return customer.getAccount().getBalance() - amount > 0;
+    }
+
+    // TODO - Denna kan göras efter att vi skriver korrekt till transaktionsloggen (metod generateStringToTransactions), då vet vi hur filen är formatterad och hur vi skall interagera med den.
     public static String findMyTransactions() {
         String transactionLog;
         // Använd currentCustomers ID som regex för att hitta alla rader i Transactions.txt som är kopplade till currentCustomer.
---
+++ src/test/java/org/example/Controller/ControllerTest.java
@@ -0,0 +1,31 @@
+package org.example.Controller;
+
+import org.example.Model.Account;
+import org.example.Model.Customer;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void successfullTransferToOtherAccountTest() {
+        Customer martin = new Customer("123","martin röv", "1", LocalDate.parse("1995-01-01"), new Account("martins konto", 5000));
+        Customer ricardoMilos = new Customer("123","ricardo milos", "1", LocalDate.parse("1993-11-15"), new Account("Ricardos konto", 5000));
+        assertTrue(Controller.transferToOtherAccount(200,martin, ricardoMilos));
+        assertEquals(4800, martin.getAccount().getBalance());
+        assertEquals(5200, ricardoMilos.getAccount().getBalance());
+    }
+
+    @Test
+    void failTransferToOtherAccountTest() {
+        Customer martin = new Customer("123","martin röv", "1", LocalDate.parse("1995-01-01"), new Account("martins konto", 0));
+        Customer ricardoMilos = new Customer("123","ricardo milos", "1", LocalDate.parse("1993-11-15"), new Account("Ricardos konto", 5000));
+        assertFalse(Controller.transferToOtherAccount(200,martin, ricardoMilos));
+        assertEquals(0, martin.getAccount().getBalance());
+        assertEquals(5000, ricardoMilos.getAccount().getBalance());
+    }
+
+}
