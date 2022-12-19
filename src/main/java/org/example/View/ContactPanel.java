c1155574479702256e45211e03f658c6fabf4a3f
alsomeb <alsomeb@gmail.com>
2022-12-19 15:54:35 +0100

    Collab session Alex, Philip, Andreas, Daniel

--- src/main/java/org/example/View/ContactPanel.java
+++ src/main/java/org/example/View/ContactPanel.java
@@ -16,6 +16,8 @@ public class ContactPanel extends JPanel {
     private String aboutHeader;
     private String aboutBody;
 
+    private JLabel ledning = new JLabel(new ImageIcon("src/main/resources/ledning.jpg"));
+
     public ContactPanel(Customer currentCustomer){
         this.currentCustomer = currentCustomer;
 
@@ -23,7 +25,7 @@ public class ContactPanel extends JPanel {
         contactInfoBody =
                 "Telefon: 072-209 14 10\n" +
                 "Email: Bankademin.support@livesupport.india\n" +
-                "Adress: Tomtebodavägen Tomtebodavägen 3A, 171 65 Solna\n\n";
+                "Adress: Tomtebodavägen 3A, 171 65 Solna\n\n";
 
         aboutHeader = "Vi är Bankademins ledningsgrupp:\n";
         aboutBody =
@@ -39,7 +41,7 @@ public class ContactPanel extends JPanel {
                 "Otto SilverHielm\n" +
                         "Otto var ordförande för studentkåren på den prestigefyllda internatskolan Solbacka.\n" +
                         "Otto brukar beskrivas av sina kollegor och vänner som ondskefull, sadistisk och hänsynslös.\n" +
-                        "Ottos fritidsintressen sträcker sig från allt kring våld och lidande till olaga hot och bajskastning.";
+                        "Ottos fritidsintressen sträcker sig från allt kring våld och lidande till olaga hot och bajskastning.\n\n";
 
 
         textArea.append(contactInfoHeader);
@@ -48,9 +50,10 @@ public class ContactPanel extends JPanel {
         textArea.append(aboutBody);
 
         textArea.setFont(new Font("Sans-serif", Font.BOLD, 18));
-        this.add(textArea);
         textArea.setEditable(false);
 
+        this.add(textArea);
+        this.add(ledning);
 
         this.setBackground(Color.WHITE);
     }
