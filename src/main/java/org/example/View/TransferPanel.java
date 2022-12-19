ee547a4bb469e262c7243db5f996198fe3c1e678
Alex <90045066+alsomeb@users.noreply.github.com>
2022-12-19 21:53:02 +0100

    Update TransferPanel.java
    removed isInteger

--- src/main/java/org/example/View/TransferPanel.java
+++ src/main/java/org/example/View/TransferPanel.java
@@ -98,8 +98,6 @@ public class TransferPanel extends JPanel {
                 JOptionPane.showMessageDialog(null,"Ange giltigt belopp! Inte minus");
             } else if (!(Controller.isDouble(amountFormatted))) {
                 JOptionPane.showMessageDialog(null, "Bara siffror tack!");
-            } else if (!(Controller.isInteger(accountFormatted))) {
-                JOptionPane.showMessageDialog(null,"Bara siffror tack!");
             } else {
                 // Kör BG/PG metoden om BG/PG är valt annars metoden för interna konton
                 if(bgPg) {
