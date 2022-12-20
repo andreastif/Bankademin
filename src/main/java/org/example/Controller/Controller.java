42ebaf9ba1feff24e641c4f2eea1773a7dae5362
andreastif <45339645+andreastif@users.noreply.github.com>
2022-12-20 09:44:25 +0100

    Cleanup

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -195,17 +195,6 @@ public class Controller {
         }
     }
 
-    /*
-    public static boolean isInteger(String number) {
-        try{
-            Integer.parseInt(number);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-    */
-
 
     public static void main(String[] args) {
         new Controller();
